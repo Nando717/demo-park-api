2785ccd05e4055fc399be27927cff140e45e3cc6
NAndo717 <iverlandioedsonn@gmail.com>
2023-11-02 19:51:58 -0300

    add timezone

--- src/main/java/com/example/demoparkapi/config/SpringTimeZoneConfig.java
+++ src/main/java/com/example/demoparkapi/config/SpringTimeZoneConfig.java
@@ -1,4 +1,15 @@
 package com.example.demoparkapi.config;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
 public class SpringTimeZoneConfig {
+    public void timezoneConfig(){
+
+        @PostConstruct
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }

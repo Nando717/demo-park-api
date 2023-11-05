658dee4730cb34f9752dc06e660af918c052816e
NAndo717 <iverlandioedsonn@gmail.com>
2023-11-05 12:12:18 -0300

    add users and atributes of users like password,name.

---
+++ src/main/java/com/example/demoparkapi/entity/Users.java
@@ -0,0 +1,39 @@
+package com.example.demoparkapi.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class Users implements Serializable {
+
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(name = "id")
+ private Long id;
+
+
+ private String username;
+ private String password;
+ private Role role;
+
+ ///registra data e hora do momento do insert do registro
+ private LocalDateTime crateData;
+
+ /// registra quando faz a modificação
+ private LocalDateTime modifyData;
+
+ /// registra o nome do usuario que fez a operação de insert na atabela
+ private String makeBy;
+
+ /// registra o usuario que fez a operação de update na atebela
+ private String modifyBy;
+
+ public enum Role{
+     ROLE_ADMIN, ROLE_CLIENT
+ }
+
+}
